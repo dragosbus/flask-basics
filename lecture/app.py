0ab788a23175b1db2d28e31af9c052ca4a53915e
dragosbus <dragosbusuioc94@gmail.com>
2018-06-18 09:46:16 +0200

    save the value on form submit on cookie and get that value

--- lecture/app.py
+++ lecture/app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, make_response, redirect
+import json
 
 
 app = Flask(__name__)
 
+
+def get_saved_data():
+    '''Get Data from cookies'''
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
+
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    data = get_saved_data()
+    return render_template('index.html', saves=data)
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    response = make_response(redirect(url_for('index')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(dict(request.form.items())))
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
